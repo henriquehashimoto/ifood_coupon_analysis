ce9ced0a115148b63176980ec66fb448bc5f4b0d
henriquehashimoto <henriquehashimoto518@gmail.com>
2025-03-13 07:19:27 -0300

    extract data from download files

--- src/data/01_data_ingestion.py
+++ src/data/01_data_ingestion.py
@@ -1,3 +1,4 @@
+from operator import contains
 import os
 import gzip
 import tarfile
@@ -9,7 +10,8 @@ from pathlib import Path
 from urllib3 import response
 
 # Define constants
-DATA_DIR = Path("data/raw")
+raw_dir = Path("data/raw")
+extract_dir = Path("data/extracted")
 URLS = {
     # 'orders.json.gz': "https://data-architect-test-source.s3-sa-east-1.amazonaws.com/order.json.gz",
     'consumers.csv.gz': "https://data-architect-test-source.s3-sa-east-1.amazonaws.com/consumer.csv.gz",
@@ -21,7 +23,7 @@ URLS = {
 #==============================
 # Download files
 #==============================
-def download_file(url: str, filename:str, dir:Path=DATA_DIR):
+def download_file(url: str, filename:str, dir:Path=raw_dir):
     filepath = dir / filename
 
     response = requests.get(url, stream=True)
@@ -34,29 +36,99 @@ def download_file(url: str, filename:str, dir:Path=DATA_DIR):
 
 
 
+#==============================
+# Extract compressed files
+#==============================
+def process_gzip_csv(file_name:str, read_path:Path=raw_dir, extract_path:Path=extract_dir):
+    """
+    Process gzipped CSV file
 
-for i in range(len(list(URLS.keys()))):
-    # print(list(URLS.keys())[i])
-    download_file(list(URLS.values())[i], list(URLS.keys())[i])
+    Parameters:
+        filename - str - name of the file to be extracted
+        read_path - path - path of the file to be read
+        extract_path - path - path of the extraction folder
+
+    Returns:
+        Extract file in the "data/extracted/" folder
+
+    """
+    path_read = read_path / file_name
+    path_extract = extract_path / file_name[:-3]
+
+    df = pd.read_csv(path_read, compression='gzip')
+    
+    return df.to_csv(path_extract, index=False)
 
 
-def process_gzip_json(filepath: Path) -> pd.DataFrame:
-    """Process gzipped JSON file"""
-    with gzip.open(filepath, 'rt', encoding='utf-8') as f:
-        data = json.load(f)
-    return pd.DataFrame(data)
+def process_gzip_json(file_name:Path, read_path:Path=raw_dir, extract_path:Path=extract_dir) -> pd.DataFrame:
+    """
+    Process gzipped JSON file
+    
+    Parameters:
+        filename - str - name of the file to be extracted
+        read_path - path - path of the file to be read
+        extract_path - path - path of the extraction folder
 
-process_gzip_json()
+    Returns:
+        Extract file in the "data/extracted/" folder
+    """
+    path_read = read_path / file_name
+    path_extract = extract_path / file_name[:-3]
+    
+    with gzip.open(path_read, 'rt', encoding='utf-8') as f:
+        df = pd.DataFrame(json.load(f))
+    
+    return df.to_json(path_extract, index=False)
 
 
-# def ensure_data_dir(data_dir: Path = DATA_DIR) -> None:
+def process_tar_csv(file_name:Path, read_path:Path=raw_dir, extract_path:Path=extract_dir):
+    """
+    Process CSV file from tar.gz
+    
+    Parameters:
+        filename - str - name of the file to be extracted
+        read_path - path - path of the file to be read
+        extract_path - path - path of the extraction folder
+
+    Returns:
+        Extract file in the "data/extracted/" folder
+    """
+    path_read = read_path / file_name
+    path_extract = extract_path / Path(str(file_name[:-6]) + 'csv')
+
+    with tarfile.open(path_read, 'r:gz') as tar:
+        csv_file = tar.getnames()[0]
+        df = pd.read_csv(tar.extractfile(csv_file), encoding='latin1')
+    
+    return print(df.head())
+
+
+
+if __name__ == "__main__":
+    for i in range(len(list(URLS.keys()))):    
+        download_file(list(URLS.values())[i], list(URLS.keys())[i]) # Reading values of dict (URL to download) and its keys (file name)
+        
+        # If is a csv tar gziped files:
+        if ".tar.gz" in list(URLS.keys())[i]:
+            process_tar_csv(list(URLS.keys())[i])
+       
+        # # If is the csv gziped files:
+        # if ".csv.gz" in list(URLS.keys())[i]:
+        #     process_gzip_csv(list(URLS.keys())[i])
+
+       
+
+
+
+
+# def ensure_raw_dir(raw_dir: Path = raw_dir) -> None:
 #     """Create data directory if it doesn't exist"""
-#     data_dir.mkdir(parents=True, exist_ok=True)
+#     raw_dir.mkdir(parents=True, exist_ok=True)
 
 
-# def download_file(url: str, filename: str, data_dir: Path = DATA_DIR) -> Path:
+# def download_file(url: str, filename: str, raw_dir: Path = raw_dir) -> Path:
 #     """Download file from URL and save to data directory"""
-#     filepath = data_dir / filename
+#     filepath = raw_dir / filename
 #     response = requests.get(url, stream=True)
 #     response.raise_for_status()
     
@@ -81,23 +153,23 @@ process_gzip_json()
 #         csv_file = tar.extractfile(tar.getmembers()[0])
 #         return pd.read_csv(csv_file)
 
-# def load_dataset(dataset_name: str, url: str, data_dir: Path = DATA_DIR) -> pd.DataFrame:
+# def load_dataset(dataset_name: str, url: str, raw_dir: Path = raw_dir) -> pd.DataFrame:
 #     """Load a specific dataset based on its type"""
 #     if dataset_name == 'orders':
-#         filepath = download_file(url, 'order.json.gz', data_dir)
+#         filepath = download_file(url, 'order.json.gz', raw_dir)
 #         return process_gzip_json(filepath)
 #     elif dataset_name in ['consumers', 'restaurants']:
-#         filepath = download_file(url, f'{dataset_name}.csv.gz', data_dir)
+#         filepath = download_file(url, f'{dataset_name}.csv.gz', raw_dir)
 #         return process_gzip_csv(filepath)
 #     elif dataset_name == 'ab_test':
-#         filepath = download_file(url, 'ab_test_ref.tar.gz', data_dir)
+#         filepath = download_file(url, 'ab_test_ref.tar.gz', raw_dir)
 #         return process_tar_csv(filepath)
 #     else:
 #         raise ValueError(f"Unknown dataset: {dataset_name}")
 
 # def load_all_datasets(urls: dict = URLS) -> dict:
 #     """Load all datasets and return them in a dictionary"""
-#     ensure_data_dir()
+#     ensure_raw_dir()
     
 #     datasets = {}
 #     for name, url in urls.items():
